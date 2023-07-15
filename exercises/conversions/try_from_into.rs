56a705aa40e550863618ad26e371a37151150618
MAnastasiaS <126902576+MAnastasiaS@users.noreply.github.com>
2023-07-15 16:21:38 +0300

    Update try_from_into.rs

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -1,13 +1,9 @@
 // try_from_into.rs
-//
-// TryFrom is a simple and safe type conversion that may fail in a controlled
-// way under some circumstances. Basically, this is the same as From. The main
-// difference is that this should return a Result type instead of the target
-// type itself. You can read more about it at
-// https://doc.rust-lang.org/std/convert/trait.TryFrom.html
-//
-// Execute `rustlings hint try_from_into` or use the `hint` watch subcommand for
-// a hint.
+// TryFrom is a simple and safe type conversion that may fail in a controlled way under some circumstances.
+// Basically, this is the same as From. The main difference is that this should return a Result type
+// instead of the target type itself.
+// You can read more about it at https://doc.rust-lang.org/std/convert/trait.TryFrom.html
+// Execute `rustlings hint try_from_into` or use the `hint` watch subcommand for a hint.
 
 use std::convert::{TryFrom, TryInto};
 
@@ -27,27 +23,43 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
 
-// Your task is to complete this implementation and return an Ok result of inner
-// type Color. You need to create an implementation for a tuple of three
-// integers, an array of three integers, and a slice of integers.
+// Your task is to complete this implementation
+// and return an Ok result of inner type Color.
+// You need to create an implementation for a tuple of three integers,
+// an array of three integers, and a slice of integers.
 //
-// Note that the implementation for tuple and array will be checked at compile
-// time, but the slice implementation needs to check the slice length! Also note
-// that correct RGB color values must be integers in the 0..=255 range.
+// Note that the implementation for tuple and array will be checked at compile time,
+// but the slice implementation needs to check the slice length!
+// Also note that correct RGB color values must be integers in the 0..=255 range.
 
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        if tuple.0 > 255 || tuple.1 > 255 || tuple.2 > 255 || tuple.0 < 0 || tuple.1 < 0 || tuple.2 < 0 {
+            return Err(IntoColorError::IntConversion)
+        } else {
+            let a = tuple.0 as u8;
+            let b = tuple.1 as u8;
+            let c = tuple.2 as u8;
+            Ok(Color {red: a, green: b, blue: c})
+        }
     }
 }
 
 // Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+    fn try_from(tuple: [i16; 3]) -> Result<Self, Self::Error> {
+        if tuple[0] > 255 || tuple[1] > 255 || tuple[2] > 255 || tuple[0] < 0 || tuple[1] < 0 || tuple[2] < 0 {
+            return Err(IntoColorError::IntConversion)
+        } else {
+            let a = tuple[0] as u8;
+            let b = tuple[1] as u8;
+            let c = tuple[2] as u8;
+            Ok(Color {red: a, green: b, blue: c})
+        }
     }
 }
 
@@ -55,6 +67,18 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen)
+        }
+        for i in slice {
+            if i < &0 || i > &255 {
+                return Err(IntoColorError::IntConversion)
+            }
+        }
+        let a = slice[0] as u8;
+        let b = slice[1] as u8;
+        let c = slice[2] as u8;
+        Ok(Color {red: a, green: b, blue: c})
     }
 }
 
