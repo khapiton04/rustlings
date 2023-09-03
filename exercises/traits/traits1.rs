2173130e9f18eff0032cf60771299d697389b35d
khapiton04 <138171279+khapiton04@users.noreply.github.com>
2023-09-03 14:25:01 +0300

    Update traits1.rs

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -1,20 +1,26 @@
 // traits1.rs
+// Time to implement some traits!
 //
-// Time to implement some traits! Your task is to implement the trait
-// `AppendBar` for the type `String`. The trait AppendBar has only one function,
-// which appends "Bar" to any object implementing this trait.
+// Your task is to implement the trait
+// `AppendBar` for the type `String`.
 //
-// Execute `rustlings hint traits1` or use the `hint` watch subcommand for a
-// hint.
+// The trait AppendBar has only one function,
+// which appends "Bar" to any object
+// implementing this trait.
+// Execute `rustlings hint traits1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+use std::ops::Add;
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
-    // TODO: Implement `AppendBar` for type `String`.
+    // TODO: Implement AppendBar for type String.
+    fn append_bar(self) -> Self {
+        self.add("Bar")
+    }
 }
 
 fn main() {
