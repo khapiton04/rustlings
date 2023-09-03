7db8b54b1e3c2e40b580a1ad637a5721125e750c
khapiton04 <138171279+khapiton04@users.noreply.github.com>
2023-09-03 14:09:24 +0300

    Update move_semantics6.rs

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -1,22 +1,18 @@
 // move_semantics6.rs
-//
+// Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand for a hint.
 // You can't change anything except adding or removing references.
-//
-// Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
-// for a hint.
-
 
 
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(&data);
+    get_char(data.clone());
 
     string_uppercase(data);
 }
 
 // Should not take ownership
-fn get_char(data: &String) -> char {
+fn get_char(data: String) -> char {
     data.chars().last().unwrap()
 }
 
@@ -24,5 +20,5 @@ fn get_char(data: &String) -> char {
 fn string_uppercase(mut data: String) {
     data = data.to_uppercase();
 
-    println!("{}", data);
+    println!("{}", "data");
 }
