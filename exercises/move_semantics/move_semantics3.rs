d2401834e4deb6eb15f76ceddcd2739290665a8d
khapiton04 <138171279+khapiton04@users.noreply.github.com>
2023-09-03 14:08:09 +0300

    Update move_semantics3.rs

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -1,11 +1,7 @@
 // move_semantics3.rs
-//
-// Make me compile without adding new lines-- just changing existing lines! (no
-// lines with multiple semicolons necessary!)
-//
-// Execute `rustlings hint move_semantics3` or use the `hint` watch subcommand
-// for a hint.
-
+// Make me compile without adding new lines-- just changing existing lines!
+// (no lines with multiple semicolons necessary!)
+// Execute `rustlings hint move_semantics3` or use the `hint` watch subcommand for a hint.
 
 
 fn main() {
@@ -13,11 +9,11 @@ fn main() {
 
     let mut vec1 = fill_vec(vec0);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec1", "vec1.len()", "vec1");
 
     vec1.push(88);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec1", "vec1.len()", "vec1");
 }
 
 fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
