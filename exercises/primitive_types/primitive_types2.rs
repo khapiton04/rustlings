a2cc87e5595809829b0630ee2f92a56bfe73d63e
khapiton04 <138171279+khapiton04@users.noreply.github.com>
2023-09-03 14:02:00 +0300

    Update primitive_types2.rs

--- exercises/primitive_types/primitive_types2.rs
+++ exercises/primitive_types/primitive_types2.rs
@@ -1,11 +1,6 @@
 // primitive_types2.rs
-//
-// Fill in the rest of the line that has code missing! No hints, there's no
-// tricks, just get used to typing these :)
-//
-// Execute `rustlings hint primitive_types2` or use the `hint` watch subcommand
-// for a hint.
-
+// Fill in the rest of the line that has code missing!
+// No hints, there's no tricks, just get used to typing these :)
 
 
 fn main() {
@@ -22,7 +17,8 @@ fn main() {
         println!("Neither alphabetic nor numeric!");
     }
 
-    let your_character = 'b'; // Finish this line like the example! What's your favorite character?
+    let your_character = 'C';
+    // Finish this line like the example! What's your favorite character?
     // Try a letter, try a number, try a special character, try a character
     // from a different language than your own, try an emoji!
     if your_character.is_alphabetic() {
