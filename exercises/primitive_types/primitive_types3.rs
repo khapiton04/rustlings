38f9fb4c53cbf244da1a5583693543766ee9658b
khapiton04 <138171279+khapiton04@users.noreply.github.com>
2023-09-03 14:02:32 +0300

    Update primitive_types3.rs

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -1,14 +1,10 @@
 // primitive_types3.rs
-//
 // Create an array with at least 100 elements in it where the ??? is.
-//
-// Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
-// for a hint.
-
+// Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
 
 fn main() {
-    let a = [0;100];
+    let a = [100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
