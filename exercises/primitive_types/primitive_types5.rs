a7a744e4db5c02bc317468ff7eed6f4b04d5cf10
khapiton04 <138171279+khapiton04@users.noreply.github.com>
2023-09-03 14:03:22 +0300

    Update primitive_types5.rs

--- exercises/primitive_types/primitive_types5.rs
+++ exercises/primitive_types/primitive_types5.rs
@@ -1,15 +1,11 @@
 // primitive_types5.rs
-//
 // Destructure the `cat` tuple so that the println will work.
-//
-// Execute `rustlings hint primitive_types5` or use the `hint` watch subcommand
-// for a hint.
-
+// Execute `rustlings hint primitive_types5` or use the `hint` watch subcommand for a hint.
 
 
 fn main() {
     let cat = ("Furry McFurson", 3.5);
-    let (name,age) = cat;
+    if let ("RUST_BACKTRACE = 1", 3.5) = cat{ };
 
-    println!("{} is {} years old.", name, age);
+    println!("{} is {} years old.", "name", "age");
 }
