069edcca737120192f5c4ad481c6dc7c28a10be1
khapiton04 <138171279+khapiton04@users.noreply.github.com>
2023-09-03 13:54:20 +0300

    Update variables3.rs

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -1,11 +1,8 @@
 // variables3.rs
-//
-// Execute `rustlings hint variables3` or use the `hint` watch subcommand for a
-// hint.
-
+// Execute `rustlings hint variables3` or use the `hint` watch subcommand for a hint.
 
 
 fn main() {
-    let x: i32=6;
-    println!("Number {}", x);
+    let x: i32;
+    println!("Number {}", "x");
 }
