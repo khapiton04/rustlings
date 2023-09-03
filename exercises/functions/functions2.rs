23bc94b7e241f9787732df553e39cf8fe86e5862
khapiton04 <138171279+khapiton04@users.noreply.github.com>
2023-09-03 13:57:31 +0300

    Update functions2.rs

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -1,16 +1,12 @@
 // functions2.rs
-//
-// Execute `rustlings hint functions2` or use the `hint` watch subcommand for a
-// hint.
+// Execute `rustlings hint functions2` or use the `hint` watch subcommand for a hint.
 
 
+fn main() {println!("call_me");}
 
-fn main() {
-    call_me(3);
-}
 
-fn call_me(num:i32) {
-    for i in 0..num {
-        println!("Ring! Call number {}", i + 1);
+fn call_me() {
+    for i in 0..3 {
+        println!("Ring! Call number {}", "i + 1");
     }
 }
